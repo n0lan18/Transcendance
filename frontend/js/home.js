7c5d41251621a6211fc33a65ec2e78fe4839d32a
Nolan Leggeri <nolanpubi@gmail.com>
2024-09-23 09:01:15 +0200

    homepage ready

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -38,15 +38,43 @@ async function fetchUserInfo()
 
 function generateBodyHomePageHTML(userInfo)
 {
+	let profilStr = "Profile";
+	let settingsStr = "Settings";
+	let soloStr = "Solo";
+	let multiplayerStr = "Multiplayer";
+	let onlineStr = "Online";
 	return `
 		<div class="flex-container">
-			<button class="flex-item box1">
-				<h1>HELLO</h1>
-				<h3>coucou</h3>
-			</button>
-			<button class="flex-item box2">2</button>
-			<button class="flex-item box3">3</button>
-			<button class="flex-item box4">4</button>
+				<button class="flex-item box1">
+					<img src="../images/profile-logo-white.png" alt="logo profile" width="70" height="70">
+					<div class="item-name"
+						<h1>${profilStr}</h1>
+					</div>
+				</button>
+				<button class="flex-item box2">
+					<img src="../images/settings-logo-white.png" alt="logo settings" width="70" height="70">
+					<div class="item-name"
+						<h1>${settingsStr}</h1>
+					</div>
+				</button>
+				<button class="flex-item box3">
+					<img src="../images/solo-logo-white.png" alt="logo solo" width="175" height="175">
+					<div class="item-name"
+						<h1>${soloStr}</h1>
+					</div>
+				</button>
+				<button class="flex-item box4">
+					<img src="../images/multiplayer-logo-white.png" alt="logo multiplayer" width="100" height="100">
+					<div class="item-name"
+						<h1>${multiplayerStr}</h1>
+					</div>
+				</button>
+				<button class="flex-item box5">
+					<img src="../images/online-logo-white.png" alt="logo online" width="100" height="100">
+					<div class="item-name"
+						<h1>${onlineStr}</h1>
+					</div>
+				</button>
 		</div>
 	`;
 }
